edc02adfeea4a3878f990e9c0375b2d975dc94c1
hadesyx <huangyxbest@163.com>
2016-08-26 11:22:48 +0800

    no message

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var utils = require('utility');
+var orm = require('orm');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    // res.render('index', { title: 'Express' });
 
-  req.models.dingdan.find({ }, function (err, dingdan) {
-    console.log(dingdan);
-    // res.render('index', { title: '订单查询', dd: dingdan });
-  });
+    var time = utils.YYYYMMDD('2016-08-24');
+    console.log(time);
+
+    req.models.dingdan.find({'createtime': orm.between('2016-08-24 10:39:17', '2016-08-24 10:39:19')}, function (err, dingdan) {
+        //console.log(dingdan);
+        res.render('index', {title: '订单查询', dd: dingdan[0]});
+    });
 });
 
 module.exports = router;
